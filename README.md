3ebcb72a28b0c0d26fdbc8521607458950884806
kt6delta <lcpreciadob@udistrital.edu.co>
2023-04-27 21:14:11 -0500

    ajustando los links

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Vue 3 + Vite + tailwind3+ docker
 
 Usa npm run dev para correr y luego abre el local host en el puerto que indica por defecto el 5173
-ademas para agregar la carpeta node_modules y las configuraciones previas usar los comandos: npm install -g vite, npm install vite --save-dev, npm install tailwindcss --save-dev, npm install autoprefixer
+ademas para agregar la carpeta node_modules y las configuraciones previas usar los comandos: npm install -g vite, npm install vite --save-dev, npm install tailwindcss --save-dev, npm install autoprefixer, npm install vue-router
 
 
 Tambien se puede ejecutar docker y luego abrir en una paguina localhost:5173 
--- src/main.js
+++ src/main.js
@@ -1,18 +1,37 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router';
 import './style.css'
-import Inicio from './Inicio.vue'
+
+import App from './App.vue'
 import Contactenos from './Contactenos.vue'
 import Alquiler from './Alquiler.vue'
-import Mantenimiento from './Mantenimiento.vue' 
+import Mantenimiento from './Mantenimiento.vue'
 import Venta from './Venta.vue'
 import Ingresar from './Ingresar.vue'
 import Registrarse from './Registrarse.vue'
 
 
-createApp(Inicio).mount('#inicio')
-createApp(Contactenos).mount('#contactenos')
-createApp(Alquiler).mount('#alquiler')
-createApp(Mantenimiento).mount('#mantenimiento')
-createApp(Venta).mount("#venta")
-createApp(Ingresar).mount("#ingresar")
-createApp(Registrarse).mount("#registrarse")
+const routes = [
+    { path: '/', name: 'Inicio', component: App },
+    { path: '/Contactenos', name: "Contactenos", component: Contactenos},
+    { path: '/Alquiler', name: "Alquiler", component: Alquiler },
+    { path: '/Mantenimiento', name: "Mantenimiento", component: Mantenimiento },
+    { path: '/Venta', name: "Venta", component: Venta },
+    { path: '/Ingresar', name: "Ingresar", component: Ingresar },
+    { path: '/Registrarse', name: "Registrarse", component: Registrarse }
+]
+const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes
+  })
+
+  export default router
+createApp(App).use(router).mount('#app')
+//createApp(Contactenos).mount('#contactenos')
+//createApp(Alquiler).mount('#alquiler')
+//createApp(Mantenimiento).mount('#mantenimiento')
+//createApp(Venta).mount("#venta")
+//createApp(Ingresar).mount("#ingresar")
+//createApp(Registrarse).mount("#registrarse")
+
+//<router-link to="/contacto" tag="button">Contacto</router-link>
