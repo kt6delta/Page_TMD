197bb1f80bcc28ef0f705fafa9d27fe996920c7f
kt6delta <lcpreciadob@correo.udistrital.edu.co>
2024-03-20 21:37:22 -0500

    se cambio de smtp google a postmark

--- utils/mail.js
+++ utils/mail.js
@@ -4,23 +4,33 @@ const nodemailer = require('nodemailer');
 import config from './config.js';
 const fs = require('fs');
 
+'use strict';
+const postmarkTransport = require('nodemailer-postmark-transport');
+
 async function sendEmail(email, username, token) {
     const msg_title = "¡Verifica tu dirección de correo electrónico!";
-    const url = config.BACKEND_URL+"/login/auth/"+token;
-    let msg_body = fs.readFileSync('utils/correo.html',{encoding:'utf8'});
+    const url = config.BACKEND_URL + "/login/auth/" + token;
+    let msg_body = fs.readFileSync('utils/correo.html', { encoding: 'utf8' });
     msg_body = msg_body.replace("{{username}}", username).replace("{{username}}", username);
     msg_body = msg_body.replace("{{url}}", url).replace("{{url}}", url).replace("{{url}}", url);
 
+    //Works en local
+    // let transporter = nodemailer.createTransport({
+    //     host: config.MAIL_SERVER,
+    //     port: config.MAIL_PORT,
+    //     secure: config.MAIL_USE_TLS,
+    //     auth: {
+    //         user: config.MAIL_USERNAME,
+    //         pass: config.MAIL_PASSWORD
+    //     }
+    // });
 
-    let transporter = nodemailer.createTransport({
-        host: config.MAIL_SERVER,
-        port: config.MAIL_PORT,
-        secure: config.MAIL_USE_TLS,
+    //Servidor SMTP privado
+    let transporter = nodemailer.createTransport(postmarkTransport({
         auth: {
-            user: config.MAIL_USERNAME,
-            pass: config.MAIL_PASSWORD
+            apiKey: config.POSTMARK_API_KEY
         }
-    });
+    }))
 
     let mailOptions = {
         from: config.MAIL_USERNAME,
@@ -41,22 +51,28 @@ async function sendEmail(email, username, token) {
 
 async function sendEmailRecuperar(email, username, token) {
     const msg_title = "¡Cambio de Contraseña!";
-    const url = config.BACKEND_URL+"/recuperar/"+token;
-    let msg_body = fs.readFileSync('utils/recuperar.html',{encoding:'utf8'});
+    const url = config.BACKEND_URL + "/recuperar/" + token;
+    let msg_body = fs.readFileSync('utils/recuperar.html', { encoding: 'utf8' });
     msg_body = msg_body.replace("{{username}}", username).replace("{{username}}", username);
     msg_body = msg_body.replace("{{url}}", url).replace("{{url}}", url).replace("{{url}}", url);
 
 
-    let transporter = nodemailer.createTransport({
-        host: config.MAIL_SERVER,
-        port: config.MAIL_PORT,
-        secure: config.MAIL_USE_TLS,
-        auth: {
-            user: config.MAIL_USERNAME,
-            pass: config.MAIL_PASSWORD
-        }
-    });
+    // let transporter = nodemailer.createTransport({
+    //     host: config.MAIL_SERVER,
+    //     port: config.MAIL_PORT,
+    //     secure: config.MAIL_USE_TLS,
+    //     auth: {
+    //         user: config.MAIL_USERNAME,
+    //         pass: config.MAIL_PASSWORD
+    //     }
+    // });
 
+    let transporter = nodemailer.createTransport(postmarkTransport({
+        auth: {
+            apiKey: config.POSTMARK_API_KEY
+        }
+    })); 
+    
     let mailOptions = {
         from: config.MAIL_USERNAME,
         to: email,
